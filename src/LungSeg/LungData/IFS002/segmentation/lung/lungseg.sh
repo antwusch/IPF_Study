459337647107044367598877e15e76ffe57f9207
antwusch <135265309+antwusch@users.noreply.github.com>
2023-06-09 09:21:08 -0700

    Delete src directory

--- src/LungSeg/LungData/IFS002/segmentation/lung/lungseg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-SUBJECT="IFS002"
-OUTPUTDIR="/home/exacloud/gscratch/BayouthLab/src/LungSeg/lungseg/../LungData//IFS002/segmentation/lung/"
-EXEDIR="/home/exacloud/gscratch/BayouthLab/src/LungSeg/lungseg-bld/bin"
